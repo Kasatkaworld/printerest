737dd78ca3a6408514fb067aa4ecb26857861187
Kasatkaworld <kasatkaworld@gmail.com>
2024-12-08 17:01:38 +0200

    item update

--- product/productJs.js
+++ product/productJs.js
@@ -1,6 +1,20 @@
-// let prod= "db.json"
-
-// async function products(){
-//     console.log(JSON.parse(JSON.stringify(prod)))
-// }
-// products()
+let Name = document.getElementById("name")
+let Price = document.getElementById("price")
+let Img = document.getElementById("img")
+let disc = document.getElementById("description")
+console.log(Img)
+let idNeeded = 1
+fetch("../db.json").then(async function(responce){
+    console.log(responce)
+    const result = await responce.json()
+    
+    for (let prod of result.products) {
+        if (idNeeded ==  prod.id) {
+            Name.innerText = prod.printer
+            Price.innerText = prod.price
+            Img.src = prod.ping
+            disc.innerText = prod.description
+        }
+        
+    }
+})
