5e8ac3a3ec9e8e049e764931a8e1a8536fd3c13c
VoidedGod0 <voidedgod0@gmail.com>
2024-12-08 17:23:53 +0200

    muchenia

--- script.js
+++ script.js
@@ -5,10 +5,15 @@ fetch("./db.json").then(async function(resonce){
     
     for(let product of result.products){
         container.innerHTML += `
-        <div class="items">
+        <div class="item">
         <h3>${product.printer}</h3>
-        <img src="${product.ping}">
+        <img src="${product.pimg}">
+        <p id="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, voluptates explicabo vel eius cupiditate alias quisquam natus officiis veniam impedit soluta rerum. A, minima repellendus velit odit repellat dicta autem.</p>
+        <a href="#"><p>Seller Profile</p></a>
+        <button>$${product.price}</button>
         </div>
         `
+        console.log(product)
     }
+    
 })
